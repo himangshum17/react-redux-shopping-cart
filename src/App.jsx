96a654b4175518195d9ed23117d72bfe057bcbe7
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-15 22:21:40 +0530

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import Cart from './pages/cart';
+import Home from './pages/home';
+
 function App() {
   return (
     <div className='container'>
       <h1 className='text-3xl font-bold underline'>
         React Redux Shopping Cart
       </h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='cart' element={<Cart />} />
+      </Routes>
     </div>
   );
 }
