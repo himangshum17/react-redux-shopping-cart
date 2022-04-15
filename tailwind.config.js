8bab0625d2ca8249e51724197b3fa123a2b98ecb
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-15 22:14:02 +0530

    add redux toolkit, tailwindcss, inter fontfamily

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
