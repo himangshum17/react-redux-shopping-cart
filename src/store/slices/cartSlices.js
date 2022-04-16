f57602fd74b32587e48b3e7ab0891bd7af20ecba
Himangshu Mondal <mailtoherock@gmail.com>
2022-04-16 20:42:17 +0530

    prevent add same product to cart

--- src/store/slices/cartSlices.js
+++ src/store/slices/cartSlices.js
@@ -5,7 +5,13 @@ export const cartSlice = createSlice({
   initialState: [],
   reducers: {
     add: (state, action) => {
-      state.push(action.payload);
+      const { id } = action.payload;
+      const duplicateProduct = state.find(obj => obj.id === id);
+      if (duplicateProduct) {
+        return state;
+      } else {
+        state.push(action.payload);
+      }
     },
     remove: (state, action) => {
       return state.filter(item => item.id !== action.payload);
